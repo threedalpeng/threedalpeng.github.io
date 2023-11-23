3a80cb86cbe7f93ef0eef0d666761c542b7debc1
Kim Jeong-won <threedalpeng@gmail.com>
2023-11-23 23:47:54 +0900

    fix: config.kit.paths.base for pointing root

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : 'https://threedalpeng.github.io/'
+			base: process.argv.includes('dev') ? '' : ''
 		}
 	}
 };
